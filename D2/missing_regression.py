862af0f768eafa3a4c0c488bfd0f5667d4e110de
Emiliano Lopez <emiliano.lopez2404@gmail.com>
2022-12-21 14:21:54 -0500

    max correlation json

--- D2/missing_regression.py
+++ D2/missing_regression.py
@@ -1,6 +1,7 @@
 import numpy as np
 import csv_to_np as ctn # support script that converts csv to np array
 from sklearn.linear_model import ElasticNet
+import json
 
 countries_data=ctn.convert('D2/filtered_countries_data.csv')
 
@@ -62,6 +63,7 @@ for i in range(1,countries_data.shape[1]):
     duplicated_countries[:, i*2] = modified_countries[:, i]
     median = float(statistics[1,i])
     
+    # assign the values of the original column to the second half of the new column
     for j in range(countries_data.shape[0]):
         if float(duplicated_countries[j, i*2])<=median:
             duplicated_countries[j,i*2+1]=0
@@ -69,9 +71,46 @@ for i in range(1,countries_data.shape[1]):
             duplicated_countries[j,i*2+1]=1
     
 
-    # assign the values of the original column to the second half of the new column
+    
     
 duplicated_countries[:,1] =  modified_countries[:,0]
 #duplicated_countries =  np.delete(modified_countries, 0, axis=1)
 duplicated_countries =  duplicated_countries[:,1:]
-ctn.revert(duplicated_countries,'duplicated_countries.csv')
+
+
+# Compute Correlations
+
+correlation_matrix=np.zeros((countries_data.shape[1],countries_data.shape[1]),dtype=object)
+
+for i in range(1,countries_data.shape[1]):
+    for j in range(1,countries_data.shape[1]):
+        corr=np.corrcoef(modified_countries[:, i].astype(float), modified_countries[:, j].astype(float))
+        correlation_matrix[i,j]=corr[0,1]
+
+corr_header=header[1:]
+correlation_matrix[:,0]=header
+correlation_matrix[0,:]=header
+ctn.revert(correlation_matrix,'correlation.csv')
+
+
+
+max_corr={}
+# to calculate the max after because we already now corr[i,i]=1
+np.fill_diagonal(correlation_matrix,0) 
+for i in range(1,correlation_matrix.shape[1]):
+    column=correlation_matrix[1:, i].astype(float)
+    index_max=np.argmax(np.abs(column))
+    print(i)
+    print(index_max)
+    value=correlation_matrix[index_max,i]
+    category=correlation_matrix[index_max,0]
+    max_corr[correlation_matrix[0,i]]={category:value}
+
+
+# Convert the list to a JSON string
+json_str = json.dumps(max_corr)
+
+# Open a file for writing
+with open("list_file.json", "w") as f:
+    # Write the JSON string to the file
+    f.write(json_str)
